b26c7147cff79c29c5db7e81e432b5deb06b663a
Alto0327 <aj.silvaf2003@gmail.com>
2025-07-17 23:40:00 -0400

    First commit

---
+++ src/components/nav.jsx
@@ -0,0 +1,18 @@
+
+export default function Nav(){
+
+    return(
+        <>
+            <nav>
+                <ul>
+                    <li>About us</li>
+                    <div className="nav-right-group">
+                        <li>Schedule</li>
+                        <li>Register Now</li>
+                    </div>
+                </ul>
+
+            </nav>
+        </>
+    )
+}
